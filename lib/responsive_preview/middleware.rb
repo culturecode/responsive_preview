da673e22bc83adb7e8880db8e9be04fe8fde3476
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2017-08-15 14:51:53 -0500

    Allow layout to use original action renderer
    This allows the layout to render partials using relative paths just like any other view.

--- lib/responsive_preview/middleware.rb
+++ lib/responsive_preview/middleware.rb
@@ -11,7 +11,11 @@ module ResponsivePreview
 
       if action_controller&.wants_responsive_preview?
         action_view = action_controller.class.helpers.tap do |context|
+          # Set the controller so helpers can use it
           context.controller = action_controller
+
+          # Set the renderer so view paths match the original controller action
+          context.view_renderer = action_controller.view_renderer
         end
 
         body = action_view.render(html: Array(response.body).join, layout: "/app/views/layouts/responsive_preview").to_s
