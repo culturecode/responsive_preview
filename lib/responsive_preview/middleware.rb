104313ee3b6a7f8ebced63d426cd33c02dc503cd
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2020-01-27 23:43:30 -0800

    Handle non Rack::Response::Body body
    Sometimes the body is an Array, and sometimes it is a String. Neither of those support calling `body` so we need to handle them separately. The Rack pattern is to either return a string or an object that yields strings when `each` is called on it.
    
    Instead, we simply make a method to call each on any non-string body and turn it into a string.

--- lib/responsive_preview/middleware.rb
+++ lib/responsive_preview/middleware.rb
@@ -6,17 +6,28 @@ module ResponsivePreview
 
     def call(env)
       request = Rack::Request.new(env)
-      status, headers, response = @app.call(env)
+      status, headers, body = @app.call(env)
       action_controller = env['action_controller.instance']
 
       if action_controller&.wants_responsive_preview?
-        original_response = Array(response.body).join
-        body = action_controller.render_to_string(html: original_response, layout: "responsive_preview")
+        original_response_body = body_to_string(body)
+        body = action_controller.render_to_string(html: original_response_body, layout: "responsive_preview")
         headers["Content-Length"] = body.bytesize.to_s
-        response = [body]
+        body = [body]
       end
 
-      return [status, headers, response]
+      return [status, headers, body]
+    end
+
+    private
+
+    def body_to_string(body)
+      case body
+      when String
+        body
+      else
+        body.each.to_a.join
+      end
     end
   end
 end
